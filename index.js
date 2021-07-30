bca2a2a629aab885c4faceaec5eacb2319b9bd3f
Madison Vallarino <madivallarino@gmail.com>
2021-07-30 15:56:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+function appendCat(name){
+   var newArray = cats.slice();
+   newArray.push(name)
+   return newArray
+}
+
+function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+
+function removeLastCat(){
+    var newArray = cats.slice();
+    newArray.pop()
+    return newArray
+}
+
+function removeFirstCat(){
+    var newArray = cats.slice();
+    newArray.shift()
+    return newArray
+}
